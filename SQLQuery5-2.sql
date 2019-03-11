237afcc3d0a054a70f62956a41ed0b127916a43e
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-11 14:18:53 +0300

    Update SQLQuery5-2.sql

--- SQLQuery5-2.sql
+++ SQLQuery5-2.sql
@@ -1,6 +1,9 @@
-SELECT   SUM(IL.UnitPrice*IL.Quantity) AS TotalSumm,  MONTH(I.InvoiceDate) AS NumberofMonth, YEAR(I.InvoiceDate) AS NumberofYEAR FROM Sales.InvoiceLines AS IL
-JOIN Sales.Invoices AS I
-ON I.InvoiceID=IL.InvoiceID
+SELECT   
+SUM(IL.UnitPrice*IL.Quantity) AS TotalSumm,  
+MONTH(I.InvoiceDate) AS NumberOfMonth, 
+YEAR(I.InvoiceDate) AS NumberOfYear 
+FROM Sales.InvoiceLines AS IL
+JOIN Sales.Invoices AS I ON I.InvoiceID=IL.InvoiceID
 GROUP BY YEAR(I.InvoiceDate), MONTH(I.InvoiceDate)
 HAVING SUM(IL.UnitPrice*IL.Quantity) > 10000
-ORDER BY  YEAR(I.InvoiceDate), MONTH(I.InvoiceDate) ASC ;
+ORDER BY  YEAR(I.InvoiceDate), MONTH(I.InvoiceDate);
