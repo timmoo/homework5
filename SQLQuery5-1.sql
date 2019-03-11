de8fa154b0ace0c06a274dad065b9fb91cf01922
timmoo <47295295+timmoo@users.noreply.github.com>
2019-03-11 14:17:50 +0300

    Update SQLQuery5-1.sql

--- SQLQuery5-1.sql
+++ SQLQuery5-1.sql
@@ -1,7 +1,10 @@
-SELECT AVG(DISTINCT S.UnitPrice) AS AveragePrice,  SUM(IL.Quantity*IL.UnitPrice) AS TotalSumm, MONTH(I.InvoiceDate) AS NumberofMonth, YEAR(I.InvoiceDate) AS NumberofYEAR  FROM Warehouse.StockItems AS S
-JOIN  Sales.InvoiceLines AS IL
-ON S.StockItemID=IL.StockItemID
-JOIN Sales.Invoices AS I
-ON I.InvoiceID=IL.InvoiceID
+SELECT 
+AVG(DISTINCT S.UnitPrice) AS AveragePrice,  
+SUM(IL.Quantity*IL.UnitPrice) AS TotalSumm, 
+MONTH(I.InvoiceDate) AS NumberOfMonth, 
+YEAR(I.InvoiceDate) AS NumberOfYear  
+FROM Warehouse.StockItems AS S
+JOIN  Sales.InvoiceLines AS IL ON S.StockItemID=IL.StockItemID
+JOIN Sales.Invoices AS I ON I.InvoiceID=IL.InvoiceID
 GROUP BY  YEAR(I.InvoiceDate), MONTH(I.InvoiceDate)
-ORDER BY  YEAR(I.InvoiceDate), MONTH(I.InvoiceDate) ASC;
+ORDER BY  YEAR(I.InvoiceDate), MONTH(I.InvoiceDate);
